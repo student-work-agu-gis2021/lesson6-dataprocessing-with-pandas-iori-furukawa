20a8b819fe44b66159ac623ec362e4136fb000d1
iorifurukawa <>
2021-05-23 16:02:22 +0000

    Calculate TWIM.

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -43,6 +43,7 @@ print('Number of no-data values in column "TAVG":',tavg_nodata_count)
 
 tmin_nodata_count = None
 #YOUR CODE HERE 3
+tmin_nodata_count=data.iloc[:,8].isnull().sum()
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
